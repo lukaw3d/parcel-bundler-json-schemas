a055bbd43ec4db24cef5dbda7831565d9288dbd5
lukaw3d <lukajeran@oasislabs.com>
2022-05-04 19:37:40 +0200

    Use draft-04 json schema

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const parcelConfigSchema = require('@parcel/core/lib/ParcelConfig.schema.js').default
 const targetDescriptorSchema = require('@parcel/core/lib/TargetDescriptor.schema.js').PACKAGE_DESCRIPTOR_SCHEMA
 
+parcelConfigSchema.title = 'JSON schema for .parcelrc files'
+parcelConfigSchema.$schema = 'http://json-schema.org/draft-04/schema#'
 parcelConfigSchema.properties.$schema = { type: 'string' }
 
 fs.writeFileSync('config_schema.json', JSON.stringify(parcelConfigSchema, null, 2), 'utf-8')
