f7db465a127a537e16a8ab0099dabd9cb637721d
lukaw3d <lukajeran@oasislabs.com>
2022-05-04 18:14:03 +0200

    Allow $schema key

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ const fs = require('fs')
 const ParcelConfigSchema = require('@parcel/core/lib/ParcelConfig.schema.js').default
 const TargetDescriptorSchema = require('@parcel/core/lib/TargetDescriptor.schema.js').PACKAGE_DESCRIPTOR_SCHEMA
 
+ParcelConfigSchema.properties.$schema = { type: 'string' }
+
 fs.writeFileSync('config_schema.json', JSON.stringify(ParcelConfigSchema, null, 2), 'utf-8')
 fs.writeFileSync('package_schema.json', JSON.stringify(TargetDescriptorSchema, null, 2), 'utf-8')
